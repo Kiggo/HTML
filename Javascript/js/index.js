b632be4800d15fc85618409ff68d3e6163622f0b
Kiggo <w2nd33@naver.com>
2022-03-08 17:05:27 +0900

    .

---
+++ Javascript/js/index.js
@@ -0,0 +1,14 @@
+var h1 = document.getElementsByTagName('h1')
+
+function changeText1(){
+h1[0].innerHTML="모각코 화이팅"
+
+}
+
+function changeText2(){
+h1[1].innerHTML="각자모여"
+}
+
+function changeText3(){
+h1[2].innerHTML="코딩하자"
+}
